e4d7e08a091e2c616fbeb75e4d98b78ef028f34a
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-08-17 10:39:45 -0600

    begin transition to class based calculator

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let firstNum;
 let secondNum;
 
-const nums = document.querySelectorAll('#num');
+const display = document.querySelector('.display');
+const numBtns = document.querySelectorAll('#num');
+const clearBtn = document.querySelector('.clear');
 
 const add = function (a, b) {
 	return a + b;
@@ -16,17 +18,72 @@ const multiply = function (a, b) {
 };
 
 const divide = function (a, b) {
+	if (b === 0) {
+		alert('Cannot divide by 0.');
+		return 'Error';
+	}
 	return a / b;
 };
 
-const operate = function (operator, a, b) {
-	return operator(a, b);
+const operate = function (operation, a, b) {
+	return operation(a, b);
 };
 
-const updateDisplay = function (val) {};
+const updateDisplay = function (val) {
+	display.textContent += val;
+};
 
-nums.forEach((num) => {
-	num.addEventListener('click', (e) => {
-		updateDispaly(e.target.value);
+const clearDisplay = function () {
+	display.textContent = 0;
+};
+
+numBtns.forEach((num) => {
+	num.addEventListener('click', () => {
+		updateDisplay(num.innerText);
 	});
 });
+
+clearBtn.addEventListener('click', clearDisplay);
+
+class Calculator {
+	constructor(equation, solution) {
+		this.equation = equation;
+		this.solution = solution;
+		this.reset = true;
+		this.clearMemory();
+	}
+
+	clearMemory() {
+		this.current = '';
+		this.previous = '';
+		this.opperation = undefined;
+	}
+
+	backspace() {
+		this.current = this.current.toString().splice(0, -1);
+	}
+
+	add(a, b) {
+		return a + b;
+	}
+
+	subtract(a, b) {
+		return a - b;
+	}
+
+	multiply(a, b) {
+		return a * b;
+	}
+
+	divide = function (a, b) {
+		if (b === 0) {
+			alert('Cannot divide by 0.');
+			return 'Error';
+		}
+		return a / b;
+	};
+
+	operate(operation, a, b) {
+		return operation(a, b);
+	}
+}
