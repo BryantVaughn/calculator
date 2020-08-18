5ee1851023d0824ea11c5ae21714b7967e6ae9d7
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-08-18 16:09:12 -0600

    styles and update README

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # JS Calculator
 
-This is the repo for my JS calculator for my final project in the web-development section of The Odin Project.
+This is a simple calculator that performs addition, subtraction, multiplication, division, and modulo operations. This is the final project for the Web Development 101 section of The Odin Project. 
+
+## Goal
+
+The goal for this assignment was to create a calculator that runs in the browser and can perform simple calculations. The calculator should be able to take multiple operations as input, but will return the result as calculated from left-to-right, not in the order of operations.
+
+## Extras
+
+Some of the stretch goals for this project were to add styles, implement a backspace button, and build out keyboard functionality, which I've done. The DEL button will remove the right-most digit from the screen, even from the result of running a calculation. I use keycodes for multiple keys to make sure users on different layout keyboards are able to use the keys they have to work with the calculator, even without a numpad.
+
+## Reflection
+
+This project definitely challenged me the most of all the projects worked on throughout the Web Development 101 curriculum. I enjoyed the challenging portions of this project that forced me to research and find better ways to implement my calculator so it worked as expected.
--- js/index.js
+++ js/index.js
@@ -7,14 +7,13 @@ class Calculator {
 	}
 
 	clearMemory() {
-		this.current = '0';
+		this.current = '';
 		this.previous = '';
 		this.opperation = undefined;
-		this.updateDisplay();
 	}
 
 	backspace() {
-		this.current = this.current.toString().splice(0, -1);
+		this.current = this.current.toString().slice(0, -1);
 	}
 
 	updateNumber(num) {
@@ -23,74 +22,54 @@ class Calculator {
 			this.current = num.toString();
 			this.reset = false;
 		} else {
-			if (num === 0 && this.current.includes('0') && this.current.length === 1)
+			if (num == 0 && this.current.includes('0') && this.current.length == 1) {
 				return;
-			else if (this.current.includes('0') && this.current.length === 1)
+			} else if (this.current.includes('0') && this.current.length == 1) {
 				this.current = num.toString();
-			else this.current = this.current.toString() + num.toString();
+			} else {
+				this.current = this.current.toString() + num.toString();
+			}
 		}
 	}
 
-	selectOperation(operation) {
+	selectOperation(op) {
 		if (this.current === '') return;
 		if (this.previous !== '') this.operate();
 
-		this.operation = operation;
+		this.operation = op;
 		this.previous = this.current;
 		this.current = '';
 	}
 
 	operate() {
-		let opResult;
-		const previous = parseFloat(this.previous);
-		const current = parseFloat(this.current);
-		if (isNaN(previous) || isNaN(current)) return;
-
+		let computation;
+		const prev = parseFloat(this.previous);
+		const curr = parseFloat(this.current);
+		if (isNaN(prev) || isNaN(curr)) return;
 		switch (this.operation) {
 			case '+':
-				opResult = parseFloat(this.add(previous, current).toFixed(4));
+				computation = parseFloat((prev + curr).toFixed(12));
 				break;
 			case '-':
-				opResult = parseFloat(this.subtract(previous, current).toFixed(4));
+				computation = parseFloat((prev - curr).toFixed(12));
 				break;
 			case 'x':
-				opResult = parseFloat(this.multiply(previous, current).toFixed(4));
+				computation = parseFloat((prev * curr).toFixed(12));
 				break;
 			case '÷':
-				opResult = parseFloat(this.divide(previous, current).toFixed(4));
+				if (curr === 0) computation = 'Err';
+				else computation = parseFloat((prev / curr).toFixed(12));
 				break;
 			case '%':
-				opResult = parseFloat(this.mod(previous, current).toFixed(4));
+				if (curr === 0) computation = 'Err';
+				else computation = parseFloat((prev % curr).toFixed(12));
 				break;
 			default:
 				return;
 		}
-
-		this.current = opResult;
+		this.current = computation;
 		this.operation = undefined;
 		this.previous = '';
-	}
-
-	add(a, b) {
-		return a + b;
-	}
-
-	subtract(a, b) {
-		return a - b;
-	}
-
-	multiply(a, b) {
-		return a * b;
-	}
-
-	divide(a, b) {
-		if (b === 0) return 'Error';
-		return a / b;
-	}
-
-	mod(a, b) {
-		if (b === 0) return 'Error';
-		return a % b;
 	}
 
 	updateDisplay() {
@@ -104,13 +83,13 @@ class Calculator {
 	}
 }
 
-const equationScrn = document.querySelector('.equation');
-const solutionScrn = document.querySelector('.solution');
-const numBtns = document.querySelectorAll('#num');
-const opBtns = document.querySelectorAll('#op');
-const equalBtn = document.querySelector('.equal');
-const clearBtn = document.querySelector('.clear');
-const backspaceBtn = document.querySelector('.delete');
+const equationScrn = document.querySelector('#equation');
+const solutionScrn = document.querySelector('#solution');
+const numBtns = document.querySelectorAll('[data-number]');
+const opBtns = document.querySelectorAll('.op');
+const equalBtn = document.querySelector('[data-equals]');
+const clearBtn = document.querySelector('[data-clear]');
+const deleteBtn = document.querySelector('[data-delete]');
 
 const calc = new Calculator(equationScrn, solutionScrn);
 
@@ -128,7 +107,7 @@ opBtns.forEach((op) => {
 	});
 });
 
-backspaceBtn.addEventListener('click', () => {
+deleteBtn.addEventListener('click', () => {
 	calc.backspace();
 	calc.updateDisplay();
 });
